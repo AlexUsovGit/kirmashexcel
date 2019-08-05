39b0eac030b0eb21de4bc7ea8c7934a071757ad4
Alexey Usov <39826412+AlexUsovGit@users.noreply.github.com>
2019-08-06 00:32:38 +0300

    Add files via upload

---
+++ src/main/java/com/example/sweater/repos/FilterRepo.java
@@ -0,0 +1,8 @@
+package com.example.sweater.repos;
+
+import com.example.sweater.domain.Filter;
+import org.springframework.data.repository.CrudRepository;
+
+public interface FilterRepo extends CrudRepository<Filter, Long> {
+
+}
